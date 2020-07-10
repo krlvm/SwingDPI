25bfd5c57658111d5594a43ab5925214ab20204d
krlvm <51774833+krlvm@users.noreply.github.com>
2020-07-10 18:16:48 +0300

    Bump version to 1.1.6

--- src/main/java/ru/krlvm/swingdpi/SwingDPI.java
+++ src/main/java/ru/krlvm/swingdpi/SwingDPI.java
@@ -19,7 +19,7 @@ import java.util.*;
  */
 public class SwingDPI {
 
-    public static final String VERSION = "1.1.5";
+    public static final String VERSION = "1.1.6";
 
     //is scale factor set
     private static boolean scaleFactorSet = false;
