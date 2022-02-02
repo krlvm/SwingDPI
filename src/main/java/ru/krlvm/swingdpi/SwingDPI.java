2a7a3044160b59c4c66ff2be84c70196d3efbbad
krlvm <51774833+krlvm@users.noreply.github.com>
2022-02-02 23:18:16 +0300

    Bump version to 1.2

--- src/main/java/ru/krlvm/swingdpi/SwingDPI.java
+++ src/main/java/ru/krlvm/swingdpi/SwingDPI.java
@@ -21,7 +21,7 @@ import java.util.List;
  */
 public class SwingDPI {
 
-    public static final String VERSION = "1.1.10";
+    public static final String VERSION = "1.2";
 
     //is scale factor set
     private static boolean scaleFactorSet = false;
